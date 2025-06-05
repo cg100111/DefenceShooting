4d21b86a424714c91416581b5a6d6da7e22cf099
gene <cg100111@gmail.com>
2025-06-06 00:36:30 +0900

    基本移動しかできない敵のプレファブを作った

--- Assets/Wu/Script/Enemy.cs
+++ Assets/Wu/Script/Enemy.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Rigidbody2D body;
+
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [SerializeField]
+    private float MoveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        //テスト
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        body.MovePosition(transform.position + MoveSpeed * Time.deltaTime * Vector3.left);
     }
 }
