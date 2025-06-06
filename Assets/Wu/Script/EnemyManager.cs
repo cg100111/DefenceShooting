9ad4698173a00e36f1bae8743e83c80e9a231f59
CI0302 <cg100111@gmail.com>
2025-06-06 14:52:47 +0900

    敵を派遣するタイミング周りの設定

--- Assets/Wu/Script/EnemyManager.cs
+++ Assets/Wu/Script/EnemyManager.cs
@@ -1,46 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+enum EnemyType
+{
+    BaseEnemy,
+    AttackEnemy
+}
 
 public class EnemyManager : MonoBehaviour
 {
+    private ObjectPool baseEnemyPool;
+
     /// <summary>
-    /// 生成する場所
-    /// </summary>
-    public Transform[] SpawnPositions;
-    /// <summary>
-    /// エネミーサンプル
-    /// </summary>
-    public GameObject EnemyPrefab;
-    /// <summary>
-    /// 生成の間隔
+    /// 派遣の間隔
     /// </summary>
     [SerializeField]
-    private float SpawnDelay;
+    private float spawnDelay;
+
     /// <summary>
-    /// 生成間隔の範囲
+    /// 派遣間隔の範囲
     /// </summary>
     [SerializeField]
-    private float SpawnDelayRange;
+    private float spawnDelayRange;
+
     /// <summary>
-    /// 生成カウンター
+    /// 派遣カウンター
     /// </summary>
-    private float SpawnCount;
+    private float deployCount;
+
+    private float deployTime;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnCount = 0;
+        Random.InitState((int)System.DateTime.Now.Ticks);
+        deployCount = 0.0f;
+        deployTime = NextDeployTime();
+        baseEnemyPool = GetComponentsInChildren<ObjectPool>().Where(c => c.CompareTag("BaseOP")).First();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void FixedUpdate()
     {
-        
+        deployCount += Time.fixedDeltaTime;
+        if(deployCount >= deployTime)
+        {
+            deployCount -= deployTime;
+            deployTime = NextDeployTime();
+            GetEnemy();
+        }
+    }
+
+    private float NextDeployTime()
+    {
+        return Random.Range(spawnDelay - spawnDelayRange, spawnDelay + spawnDelayRange);
+    }
+
+    private void GetEnemy()
+    {
+
+    }
+
+    public void RecycleAllEnemy()
+    {
+       
     }
 }
