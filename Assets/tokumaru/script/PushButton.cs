da80c44b1f67171c49eb99ed4a66dbe5a70cd9e9
tokumaru3325 <furufuru3325@gmail.com>
2025-06-10 12:30:34 +0900

    シーン切り替え

---
+++ Assets/tokumaru/script/PushButton.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PushButton : MonoBehaviour
+{
+    public Image image;
+    Color color;
+    bool pushButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = image.color;
+        pushButton = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pushButton) {
+            FlashAndSceneChange(2);
+        }
+    }
+
+    public void OnButtonClick()
+    {
+        pushButton = true;
+        //SceneManager.LoadScene("Game");
+    }
+    void FlashAndSceneChange(int time_)
+    {
+
+        if(time_ >= 0)
+        {
+            if ((time_ / 4) % 2 == 1)
+            {
+                color.a = 1.0f;
+                image.color = color;
+            }
+            else
+            {
+                color.a = 0.0f;
+                image.color = color;
+            }
+            time--;
+        }
+        else
+        {
+            return;
+        }
+
+
+    }
+}
--- Assets/tokumaru/script/TextAppear.cs
+++ Assets/tokumaru/script/TextAppear.cs
@@ -7,12 +7,14 @@ public class TextAppear : MonoBehaviour
 {
     public Image image;
     Color color;
+    float addAlpha;
     // Start is called before the first frame update
     void Start()
     {
         color = image.color;
         color.a = 0.0f;
         image.color = color;
+        addAlpha = 0.01f;
     }
 
     // Update is called once per frame
@@ -20,8 +22,7 @@ public class TextAppear : MonoBehaviour
     {
         if (color.a < 1.0f)
         {
-            Debug.Log("non");
-            color.a += 0.001f;
+            color.a += addAlpha;
             image.color = color;
         }
     }
