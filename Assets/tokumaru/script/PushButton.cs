2787ff9a93dc07829fe53c6a50e0d6bed3f67236
tokumaru3325 <furufuru3325@gmail.com>
2025-06-11 10:56:48 +0900

    フェード、ボタンのアニメーション

--- Assets/tokumaru/script/PushButton.cs
+++ Assets/tokumaru/script/PushButton.cs
@@ -7,50 +7,44 @@ using UnityEngine.UI;
 public class PushButton : MonoBehaviour
 {
     public Image image;
+    public Animator animator;
     Color color;
     bool pushButton;
+    float timer;
+    float limit;
+    public bool sceneChange;
 
     // Start is called before the first frame update
     void Start()
     {
         color = image.color;
+        animator = gameObject.GetComponent<Animator>();
         pushButton = false;
+        timer = 0.0f;
+        limit = 30.0f;
+        sceneChange = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(pushButton) {
-            FlashAndSceneChange(2);
+        if (pushButton)
+        {
+            if (timer >= limit)
+            {
+                animator.SetBool("OnPush", false);
+                pushButton = false;
+                sceneChange = true;
+                Debug.Log("animator,PushButton false");
+            }
+            timer += 1.0f;
         }
     }
 
     public void OnButtonClick()
     {
         pushButton = true;
-        //SceneManager.LoadScene("Game");
-    }
-    void FlashAndSceneChange(int time_)
-    {
-
-        if(time_ >= 0)
-        {
-            if ((time_ / 4) % 2 == 1)
-            {
-                color.a = 1.0f;
-                image.color = color;
-            }
-            else
-            {
-                color.a = 0.0f;
-                image.color = color;
-            }
-        }
-        else
-        {
-            return;
-        }
-
-
+        animator.SetBool("OnPush", true);
+        Debug.Log("animator,PushButton true");
     }
 }
