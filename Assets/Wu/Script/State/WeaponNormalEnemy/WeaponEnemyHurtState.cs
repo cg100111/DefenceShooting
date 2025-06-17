9c398b68572dd655fe0997c4efd165c9ca094081
CI0302 <cg100111@gmail.com>
2025-06-17 10:40:08 +0900

    一般の敵と武器持っている敵とその状態を作った

---
+++ Assets/Wu/Script/State/WeaponNormalEnemy/WeaponEnemyHurtState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponEnemyHurtState : BaseState
+{
+    public WeaponEnemyHurtState(Character c, StateManager m) : base(c, m) { }
+
+    public override void EnterState()
+    {
+        Enemy mine = (Enemy)me;
+        if (mine.hitDir == Enemy.HitDir.front)
+            mine.GetAnimator().CrossFadeInFixedTime("GetHitFront", 0f);
+        else
+            mine.GetAnimator().CrossFadeInFixedTime("GetHitBack", 0f);
+        mine.PlayHitSE();
+        mine.GetBody().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    public override void ExitState()
+    {
+        Enemy mine = (Enemy)me;
+        mine.GetBody().constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    public override void UpdateState(Character target)
+    {
+        Enemy mine = (Enemy)me;
+        if (!mine.isHit)
+        {
+            manager.ChangeState(new EnemyWalkState(me, manager));
+        }
+    }
+}
