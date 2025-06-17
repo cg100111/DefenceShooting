19509582850101e706adb96c5bccae524bbbbab7
Sorasora520 <135576592+Sorasora520@users.noreply.github.com>
2025-06-17 11:43:42 +0900

    ビルドした後にスタートボタンの位置が変化する問題を修正

--- Assets/Takimoto/Title/Script/Start_Button.cs
+++ Assets/Takimoto/Title/Script/Start_Button.cs
@@ -7,11 +7,13 @@ using UnityEngine.SceneManagement;
 public class Start_Button : MonoBehaviour
 {
     public FadeSceneLoader fadeSceneLoader;
+    public Vector2 position = new Vector2(400, 40);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.anchoredPosition = position;
     }
 
     // Update is called once per frame
