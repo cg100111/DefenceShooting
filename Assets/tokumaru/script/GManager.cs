46f525cc68c4876da7f45158a0481bc0e251a854
tokumaru3325 <furufuru3325@gmail.com>
2025-06-18 11:37:12 +0900

    no message

--- Assets/tokumaru/script/GManager.cs
+++ Assets/tokumaru/script/GManager.cs
@@ -103,7 +103,6 @@ public class GManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            UnityEditor.EditorApplication.isPlaying = false;
             Application.Quit();
         }
     }
